c18824faa0233266780540e51f09ae331c171eea
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-01-19 18:20:32 +0100

    ,,

---
+++ Aoraaa/app/_layout.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import "../global.css";
+
+const RootLayout = () => {
+  return (
+    <View>
+      <Text>_layout</Text>
+    </View>
+  );
+};
+
+export default RootLayout;
